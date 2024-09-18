721b1d7a4bd8a834d0f5c8ee830f6020164818b5
Anthony Griffon <anthony+w1@griffon.one>
2024-09-18 14:39:16 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as BunnySDK from "https://esm.sh/@bunny.net/edgescript-sdk@0.11.1";
+import * as UA from "https://esm.sh/ua-parser-js@1.0.39";
+
+console.log("Starting server...");
+
+BunnySDK.net.http.servePullZone({ url: "https://echo.free.beeceptor.com/" })
+  .onOriginRequest(
+    async (ctx) => {
+      ctx.request.headers.set("mobile", "true");
+      const ua = ctx.request.headers.get("User-Agent");
+      const parser = new UA.UAParser(ua);
+      const device = parser.getDevice();
+      switch (device) {
+        case "mobile":
+          ctx.request.headers.set("isMobile", "true");
+          break;
+        case "tablet":
+          ctx.request.headers.set("isTablet", "true");
+          break;
+        default:
+          break;
+      }
+    },
+  ).onOriginResponse(async (ctx) => {
+    const body = await ctx.response.json();
+    body.ip = "Redacted";
+    body.headers["User-Agent"] = "RedactedUA";
+    return new Response(JSON.stringify(body, null, 2));
+  });
